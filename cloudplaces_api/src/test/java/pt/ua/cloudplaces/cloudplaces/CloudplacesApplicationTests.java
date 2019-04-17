55ac6ee355192cffbc4fcd3bf816d1eed47b41ac
Rafael Direito <rafael.neves.direito@ua.pt>
2019-04-17 23:29:30 +0100

    tests updated

--- cloudplaces_api/src/test/java/pt/ua/cloudplaces/cloudplaces/CloudplacesApplicationTests.java
+++ cloudplaces_api/src/test/java/pt/ua/cloudplaces/cloudplaces/CloudplacesApplicationTests.java
@@ -1,12 +1,13 @@
 package pt.ua.cloudplaces.cloudplaces;
 
+import cloudplaces.restapi.CloudplacesApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = CloudplacesApplication.class)
 public class CloudplacesApplicationTests {
 
 	@Test
