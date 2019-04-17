abc629e0b3ac6f0da23f4686291c38ebc96a75cf
Rafael Direito <rafael.neves.direito@ua.pt>
2019-04-17 23:17:00 +0100

    Spring Rest Api + Swagger

---
+++ cloudplaces_api/src/test/java/pt/ua/cloudplaces/cloudplaces/CloudplacesApplicationTests.java
@@ -0,0 +1,16 @@
+package pt.ua.cloudplaces.cloudplaces;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudplacesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
