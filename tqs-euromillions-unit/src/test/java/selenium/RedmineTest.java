447d6f230f7225219012a8bf1325a0ae49063d62
Rafael Direito <rafael.neves.direito@ua.pt>
2019-06-23 12:36:55 +0100

    selenium tests

--- tqs-euromillions-unit/src/test/java/selenium/RedmineTest.java
+++ tqs-euromillions-unit/src/test/java/selenium/RedmineTest.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package selenium;
 
 /**
@@ -18,74 +18,82 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.Select;
-        
+
 public class RedmineTest {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
-
-  @BeforeEach
-  public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "https://www.katalon.com/";
-    driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testRedmine() throws Exception {
-    driver.get("http://demo.redmine.org/");
-    driver.findElement(By.linkText("Sign in")).click();
-    driver.findElement(By.id("username")).clear();
-    driver.findElement(By.id("username")).sendKeys("rd");
-    driver.findElement(By.id("password")).clear();
-    driver.findElement(By.id("password")).sendKeys("rdrd");
-    driver.findElement(By.name("login")).click();
-    driver.findElement(By.linkText("Sign out")).click();
-    assertEquals("Sign in", driver.findElement(By.linkText("Sign in")).getText());
-  }
-
-  @AfterEach
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+    
+    @BeforeEach
+    public void setUp() throws Exception {
+        // Run without display
+        FirefoxBinary firefoxBinary = new FirefoxBinary();
+        firefoxBinary.addCommandLineOptions("--headless");
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.setBinary(firefoxBinary);
+        
+        driver = new FirefoxDriver(firefoxOptions);
+        baseUrl = "https://www.katalon.com/";
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
     }
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
+    
+    @Test
+    public void testRedmine() throws Exception {
+        driver.get("http://demo.redmine.org/");
+        driver.findElement(By.linkText("Sign in")).click();
+        driver.findElement(By.id("username")).clear();
+        driver.findElement(By.id("username")).sendKeys("rd");
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("rdrd");
+        driver.findElement(By.name("login")).click();
+        driver.findElement(By.linkText("Sign out")).click();
+        assertEquals("Sign in", driver.findElement(By.linkText("Sign in")).getText());
     }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
+    
+    @AfterEach
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
     }
-  }
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
+    
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
-  }
+    
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+    
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
 }
 ;
