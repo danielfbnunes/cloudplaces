466ff1b9e60660ca340bfbb137b669058e5b8d37
Daniel Nunes <danielnunes98@ua.pt>
2019-06-24 20:15:58 +0100

    Added android base app

---
+++ CloudPlacesAndroidApp/app/src/main/java/pt/ua/cloudplacesandroidapp/Activities/Register.java
@@ -0,0 +1,95 @@
+package pt.ua.cloudplacesandroidapp.Activities;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import pt.ua.cloudplacesandroidapp.R;
+
+public class Register extends AppCompatActivity {
+    // UI references.
+    private static final int SELECT_PHOTO = 100;
+    private ImageView chooseImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button regist = (Button) findViewById(R.id.register_btn_confirm);
+        regist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptRegister();
+            }
+        });
+
+        chooseImage=(ImageView) findViewById(R.id.imageView2);
+        Button pickImage = (Button) findViewById(R.id.pickImage);
+        pickImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChooseImage(v);
+            }
+        });
+        // Don't show keyboard on beginning
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
+    public void ChooseImage(View v){
+        openGallery();
+    }
+
+    private void openGallery(){
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode) {
+            case SELECT_PHOTO:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = data.getData();
+                    if(selectedImage !=null){
+                        chooseImage.setImageURI(selectedImage);
+                    }
+                }
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent myIntent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivityForResult(myIntent, 0);
+        return true;
+    }
+
+    private void attemptRegister() {
+        //TODO: validate fields
+        startActivity(new Intent(Register.this, Welcome.class));
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Do Here what ever you want do on back press;
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        // refresh your views here
+        super.onConfigurationChanged(newConfig);
+    }
+}
---
+++ CloudPlacesAndroidApp/app/src/main/java/pt/ua/cloudplacesandroidapp/Fragments/AccommodationFragment.java
@@ -0,0 +1,75 @@
+package pt.ua.cloudplacesandroidapp.Fragments;
+
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
+
+import pt.ua.cloudplacesandroidapp.R;
+
+
+public class AccommodationFragment extends Fragment {
+
+    private String name;
+    private String accomodation;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        DrawerLayout drawer = (DrawerLayout) getActivity().findViewById(R.id.drawer_layout);
+        final Toolbar toolbar = (Toolbar)  getActivity().findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().onBackPressed();
+            }
+        });
+
+        View view = inflater.inflate(R.layout.fragment_accommodation, container, false);
+
+        ImageView accommodationImage = (ImageView)  view.findViewById(R.id.accommodationImage);
+        TextView accommodationName = (TextView) view.findViewById(R.id.accommodationName);
+        TextView accommodationDescription = (TextView) view.findViewById(R.id.accommodationDescription);
+
+        // Load the images into the ImageView using the Glide library.
+        Glide.with(getContext().getApplicationContext()).load("https://i.kinja-img.com/gawker-media/image/upload/s--bIV3xkEm--/c_scale,f_auto,fl_progressive,q_80,w_800/jsprifdd1gmfy7e7nola.jpg").into(accommodationImage);
+        // Load textviews
+        accommodationName.setText(name);
+
+        //TODO: add more info about the current accommodation
+        //accommodationDescription.setText(bio);
+
+        return view;
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle(name);
+    }
+
+
+    public AccommodationFragment initializeData(String accomodation)
+    {
+
+        this.accomodation = accomodation;
+        this.name = accomodation;
+        //TODO: add more info about the current accommodation
+        //this.bio=p.getBio();
+        return this;
+    }
+}
