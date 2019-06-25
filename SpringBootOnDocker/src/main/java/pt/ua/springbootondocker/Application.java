8969a5d603c6ffd3daf57ef8abc2c368ee43b9ca
Rafael Direito <rafael.neves.direito@ua.pt>
2019-06-25 10:40:32 +0100

    test deployment on jenkins

---
+++ SpringBootOnDocker/src/main/java/pt/ua/springbootondocker/Application.java
@@ -0,0 +1,29 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+package pt.ua.springbootondocker;
+
+import java.util.Collections;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+    
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+    
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "80"));
+        app.run(args);
+    }
+    
+}
