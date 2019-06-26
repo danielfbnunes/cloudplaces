fd5526bc68a14b235e0f21e6ceb7612c48e898ad
Rafael Teixeira <rafaelgteixeira@ua.pt>
2019-06-26 18:51:54 +0100

    Reconfig webdriver

--- CloudPlaces/src/test/java/cloudplaces/cucumber/Stepdefs.java
+++ CloudPlaces/src/test/java/cloudplaces/cucumber/Stepdefs.java
@@ -4,9 +4,12 @@ import cloudplaces.webapp.CloudPlacesApplication;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
@@ -17,7 +20,22 @@ public class Stepdefs {
     //Example
     @Given("John as already logged in")
     public void test1(){
-        WebDriver driver = new FirefoxDriver();
+        Map<String, String> environment = new HashMap<>();
+        WebDriver driver;
+        // check if a GUI is available
+        if(System.getenv("DISPLAY") == null || System.getenv("DISPLAY").equals(":99")){
+           System.out.println("Setting Up a Display");
+           environment.put("DISPLAY", ":99");
+        }
+        else System.out.println("Using computer’s defaul GUI");
+
+        GeckoDriverService service = new GeckoDriverService.Builder()
+        .usingAnyFreePort()
+        .withEnvironment(environment)
+        .build();;
+
+        driver = new FirefoxDriver(service);
+        
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.get("http://localhost:8080/getIndex");
         driver.quit();
