04183f2236b69d815fdfb412975b96802c3d7b9e
Rafael Direito <rafael.neves.direito@ua.pt>
2019-06-28 15:32:26 +0000

    Stepdefs.java edited online with Bitbucket

--- CloudPlaces/src/test/java/cloudplaces/webapp/Stepdefs.java
+++ CloudPlaces/src/test/java/cloudplaces/webapp/Stepdefs.java
@@ -27,7 +27,7 @@ public class Stepdefs {
     private final String baseUrl = "http://localhost:8080/";
     private boolean acceptNextAlert = true;
     private final StringBuffer verificationErrors = new StringBuffer();
-    private final long waitingTimer = 2000;
+    private final long waitingTimer = 4000;
     
     @Autowired
     UserQueries uq;
