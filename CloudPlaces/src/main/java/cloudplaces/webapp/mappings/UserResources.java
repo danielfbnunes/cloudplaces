edb27f75e75478af07ac4b40744d8ee9c738045c
Daniel Nunes <danielnunes98@ua.pt>
2019-06-29 19:02:54 +0100

    Authentication throught api

--- CloudPlaces/src/main/java/cloudplaces/webapp/mappings/UserResources.java
+++ CloudPlaces/src/main/java/cloudplaces/webapp/mappings/UserResources.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -34,6 +35,12 @@ public class UserResources {
     UserQueries query;
     
     private final String errorMessage = "Error";
+    
+    @ApiOperation("Authenticate user")
+    @PostMapping("api/authenticate")
+    public Object authenticateUser(@RequestBody Map<String, String> credentials){
+        return query.authenticateUser(credentials.get("email"), credentials.get("password"));
+    }
     
     @ApiOperation("Inserts a user in database")//TODO Fix api call
     @PostMapping("api/add_user/{name}/{email}/{pw}/{cellphone}/{photo}")
